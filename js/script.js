0df6ea22c72ccdd0d5e4cf1ccb2ac1e8d7698fab
Bhumika Amarjeeth <bamarjee@asu.edu>
2020-05-13 23:43:07 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+
+//Navbar
+
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
+// Hamburgur menu
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+
+
+//Testimonials
+jQuery(document).ready(function($) {
+        		"use strict";
+        		//  TESTIMONIALS CAROUSEL HOOK
+		        $('#customers-testimonials').owlCarousel({
+		            loop: true,
+		            center: true,
+		            items: 3,
+		            margin: 0,
+		            autoplay: true,
+		            dots:true,
+		            autoplayTimeout: 2500,
+		            smartSpeed: 450,
+		            responsive: {
+		              0: {
+		                items: 1
+		              },
+		              768: {
+		                items: 2
+		              },
+		              1170: {
+		                items: 3
+		              }
+		            }
+		        });
+        	});
